e95d1badb08ef8da39b7f8750bc454b531975150
Rich Infante <rich@richinfante.com>
2019-11-21 09:40:53 -0500

    Fix atoi

--- fb-clearscreen.c
+++ fb-clearscreen.c
@@ -28,7 +28,7 @@ int main(int argc, char* argv[]) {
        unsigned char color = 0x0;
         for (int i = 0; i < argc; i++) {
             if (strcmp(argv[i], "-g") == 0 && i+1 < argc) {
-                color = aroi(argv[i+1]) & 0xFF;
+                color = atoi(argv[i+1]) & 0xFF;
             }
         }
 
